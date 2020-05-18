d07eb8e7d8f6694c01ba532817a62e7746448373
Rasmus <rasmus.hassenkam@gmail.com>
2020-05-18 19:40:41 +0200

    added shooting logic

--- Player.gd
+++ Player.gd
@@ -8,6 +8,9 @@ var selected_weapon = weapons[0]
 
 var velocity = Vector2()
 var rotation_dir = 0
+var Bullet = preload("res://Bullet.tscn")
+
+signal shoot(type, direction, location)
 
 func _ready():
 	print("ready")
@@ -25,11 +28,15 @@ func get_input():
 		velocity = Vector2(speed, 0).rotated(rotation)
 		if selected_weapon == weapons[0]:
 			$AnimatedSprite.play("walk")
+		#elif selected_weapon == weapons[1]:
+			#$AnimatedSprite.play("nightstick_move")
 		elif selected_weapon == weapons[1]:
 			$AnimatedSprite.play("handgun_run_n_gun")
 	else:
 		if selected_weapon == weapons[0]:
 			$AnimatedSprite.play("unarmed_idle")
+		#elif selected_weapon == weapons[1]:
+			#$AnimatedSprite.play("nightstick_idle")
 		elif selected_weapon == weapons[1]:
 			$AnimatedSprite.play("handgun_idle")
 		
@@ -41,13 +48,17 @@ func _physics_process(delta):
 
 func switch_weapons():
 	if Input.is_action_just_pressed("prev_weapon"):
-		print("next")
 		if selected_weapon == weapons[weapons.size() - 1]:
 			selected_weapon = weapons[0]
 		else:
 			var index = weapons.find(selected_weapon)
 			selected_weapon = weapons[index + 1]
+			
+func shoot():
+	if Input.is_action_just_pressed("attack"):
+		if selected_weapon == weapons[1]:
+			emit_signal("shoot", Bullet, rotation, position)
 		
 func _process(delta):
+	shoot()
 	switch_weapons()
-	print(selected_weapon)
