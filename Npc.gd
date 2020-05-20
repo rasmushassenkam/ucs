3fe20b8108e05fb2b2048e1e64546a2e76008c18
Rasmus <rasmus.hassenkam@gmail.com>
2020-05-20 21:38:58 +0200

    added npc death animation

--- Npc.gd
+++ Npc.gd
@@ -1,10 +1,17 @@
 extends KinematicBody2D
 
-var health = 100
+var health = 30
+
+func _ready():
+	$AnimatedSprite.play("npc01_idle")
 
 func _process(delta):
 	if health <= 0:
-		queue_free()
+		death()
 
 func take_damage(dmgTaken):
 	health -= dmgTaken
+	
+func death():
+	$AnimatedSprite.play("npc01_death")
+	$CollisionShape2D.queue_free()
